341994585a53662be51790c74d501db8d587c88c
Vishesh Singh <VisheshSingh16@gmail.com>
2024-05-18 13:02:40 -0400

    Testing input with fetch call

---
+++ src/App.tsx
@@ -0,0 +1,196 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function getAutoCompleteResults(query: string): Promise<string[]> {
+  const fruits = [
+    'Abiu',
+    'Açaí',
+    'Acerola',
+    'Akebi',
+    'Ackee',
+    'African Cherry Orange',
+    'American Mayapple',
+    'Apple',
+    'Apricot',
+    'Aratiles',
+    'Araza',
+    'Avocado',
+    'Banana',
+    'Bilberry',
+    'Blackberry',
+    'Blackcurrant',
+    'Black sapote',
+    'Blueberry',
+    'Boysenberry',
+    'Breadfruit',
+    'Cactus pear',
+    'Canistel',
+    'Catmon',
+    'Cempedak',
+    'Cherimoya',
+    'Cherry',
+    'Chico fruit',
+    'Cloudberry',
+    'Coco de mer',
+    'Coconut',
+    'Crab apple',
+    'Cranberry',
+    'Currant',
+    'Damson',
+    'Date',
+    'Dragonfruit ',
+    'Durian',
+    'Elderberry',
+    'Feijoa',
+    'Fig',
+    'Finger ',
+    'Gac',
+    'Goji berry',
+    'Gooseberry',
+    'Grape',
+    'Raisin',
+    'Grapefruit',
+    'Grewia asiatica',
+    'Guava',
+    'Hala fruit',
+    'Haws',
+    'Honeyberry',
+    'Huckleberry',
+    'Jabuticaba',
+    'Jackfruit',
+    'Jambul',
+    'Japanese plum',
+    'Jostaberry',
+    'Jujube',
+    'Juniper berry',
+    'Kaffir lime',
+    'Kiwano ',
+    'Kiwifruit',
+    'Kumquat',
+    'Lanzones',
+    'Lemon',
+    'Lime',
+    'Loganberry',
+    'Longan',
+    'Loquat',
+    'Lulo',
+    'Lychee',
+    'Magellan Barberry',
+    'Macopa ',
+    'Mamey apple',
+    'Mamey Sapote',
+    'Mango',
+    'Mangosteen',
+    'Marionberry',
+    'Medlar',
+    'Melon',
+    'Cantaloupe',
+    'Galia melon',
+    'Honeydew',
+    'Mouse melon',
+    'Muskmelon',
+    'Watermelon',
+    'Miracle fruit',
+    'Momordica fruit',
+    'Monstera deliciosa',
+    'Mulberry',
+    'Nance',
+    'Nectarine',
+    'Orange',
+    'Blood orange',
+    'Clementine',
+    'Mandarine',
+    'Tangerine',
+    'Papaya',
+    'Passionfruit',
+    'Pawpaw',
+    'Peach',
+    'Pear',
+    'Persimmon',
+    'Plantain',
+    'Plum',
+    'Prune',
+    'Pineapple',
+    'Pineberry',
+    'Plumcot',
+    'Pomegranate',
+    'Pomelo',
+    'Quince',
+    'Raspberry',
+    'Salmonberry',
+    'Rambutan',
+    'Redcurrant',
+    'Rose apple',
+    'Salal berry',
+    'Salak',
+    'Santol',
+    'Sapodilla',
+    'Sapote',
+    'Saquico',
+    'Sarguelas',
+    'Satsuma',
+    'Sloe',
+    'Soursop',
+    'Star apple',
+    'Star fruit',
+    'Strawberry',
+    'Sugar apple',
+    'Suriname cherry',
+    'Tamarillo',
+    'Tamarind',
+    'Tangelo',
+    'Tayberry',
+    'Thimbleberry',
+    'Ugli fruit',
+    'White currant',
+    'White sapote',
+    'Ximenia',
+    'Yuzu',
+  ];
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(
+        fruits.filter((fruit) =>
+          fruit.toLowerCase().includes(query.toLowerCase())
+        )
+      );
+    }, Math.random() * 1000);
+  });
+}
+
+function App() {
+  const [query, setQuery] = useState<string>('');
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+
+  useEffect(() => {
+    setSuggestions([]);
+    const fetchFruits = async () => {
+      const data = await getAutoCompleteResults(query);
+      setSuggestions(data);
+    };
+
+    if (!query) {
+      setSuggestions([]);
+    }
+
+    if (query.length) {
+      fetchFruits();
+    }
+  }, [query]);
+
+  return (
+    <>
+      <input
+        type='text'
+        placeholder='Search here...'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {suggestions.length > 0 &&
+        suggestions.map((suggestion) => <p key={suggestion}>{suggestion}</p>)}
+    </>
+  );
+}
+
+export default App;
